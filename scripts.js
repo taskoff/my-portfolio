d30ccb91a5fac0895b7b5a011e87b9c5a2c5ad27
taskoff <marian_taskov@abv.bg>
2020-10-02 14:14:02 +0300

    many changes :)

--- scripts.js
+++ scripts.js
@@ -4,6 +4,28 @@ import {firstSliderInit} from './scripts/slider.js';
 
 firstSliderInit();
 
+function showWorkCategory(){
+    const links = document.querySelectorAll('.type-of-work');
+    links.forEach(e=>e.addEventListener('click', showCategory));
+    const projects = document.querySelectorAll('.work-container');
+
+    function showCategory(e){
+        console.log(e.target.getAttribute('value'))
+        const cls = e.target.getAttribute('value');
+        projects.forEach(e=>{
+            if(cls==='all'){
+                e.classList.remove('hidden');
+            }else if(!e.className.includes(cls)) {
+                e.classList.add('hidden');
+            } else {
+                e.classList.remove('hidden');
+            };
+        })
+        
+    }
+}
+showWorkCategory()
+
 
 let position = 0;
 
